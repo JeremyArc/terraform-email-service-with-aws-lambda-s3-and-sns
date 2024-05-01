60e76752d57f3a687408fb7ff8ddaec5a5d4fa7a
RathomBoi <jessada.srm@gmail.com>
2024-05-01 18:07:44 +0700

    add more dependency

--- src/deployment/main.tf
+++ src/deployment/main.tf
@@ -104,10 +104,15 @@ resource "aws_s3_bucket_notification" "bucket_notification" {
   lambda_function {
     lambda_function_arn = aws_lambda_function.Lambda-Emailing-SNS.arn
     events              = ["s3:ObjectCreated:*"]
+    filter_prefix = "email"
+    filter_suffix = ".csv"
   }
 
 # wait until give permission to S3 finished
-  depends_on = [ aws_lambda_permission.allow_s3_to_invoke_lambda ]
+  depends_on = [ 
+      aws_s3_bucket.lambda-emailing-service, 
+      aws_lambda_permission.allow_s3_to_invoke_lambda 
+      ]
 
 }
 
