184f2c4768d76406871883b535cb14816bdccf0d
RathomBoi <jessada.srm@gmail.com>
2024-05-01 17:29:00 +0700

    revise structure codes

--- src/deployment/main.tf
+++ src/deployment/main.tf
@@ -73,15 +73,15 @@ data "aws_iam_role" "Lambda-Emailing-Role" {
 }
 
 # Lambda function
+# Lambda function
 resource "aws_lambda_function" "Lambda-Emailing-SNS" {
-  function_name = var.FUNCTION_NAME
-  handler = "lambda_function.handler"
-  runtime = "python3.8"
-  role = data.aws_iam_role.Lambda-Emailing-Role.arn
-
+  function_name    = var.FUNCTION_NAME
+  handler          = "lambda_function.handler"
+  runtime          = "python3.8"
+  role             = data.aws_iam_role.Lambda-Emailing-Role.arn
+  filename         = "${path.module}/../function/email-service.zip"
   source_code_hash = filebase64sha256("${path.module}/../function/email-service.zip")
-  filename = "${path.module}/../function/email-service.zip"
-  
+
   environment {
     variables = {
       SNS_TOPIC_ARN = aws_sns_topic.new-product-topic.arn
@@ -98,25 +98,21 @@ resource "aws_lambda_permission" "allow_s3_to_invoke_lambda" {
   source_arn    = aws_s3_bucket.lambda-emailing-service.arn
 }
 
-# S3 bucket
+# Simplified S3 bucket notification configuration
+resource "aws_s3_bucket_notification" "bucket_notification" {
+  bucket = aws_s3_bucket.lambda-emailing-service.id
+  lambda_function {
+    lambda_function_arn = aws_lambda_function.Lambda-Emailing-SNS.arn
+    events              = ["s3:ObjectCreated:*"]
+  }
+}
+
+# S3 bucket creation
 resource "aws_s3_bucket" "lambda-emailing-service" {
   bucket = var.BUCKET_NAME
 
   tags = {
     Name        = var.TAG_NAME
     Environment = var.TAG_ENV
-  }
-}
-
-# bucket notification
-resource "aws_s3_bucket_notification" "bucket_notification" {
-  bucket = aws_s3_bucket.lambda-emailing-service.id
-
-  lambda_function {
-    lambda_function_arn = aws_lambda_function.Lambda-Emailing-SNS.arn
-    # Docs: https://docs.aws.amazon.com/AmazonS3/latest/userguide/notification-how-to-event-types-and-destinations.html#supported-notification-event-types
-    events = ["s3:ObjectCreated:*"]
-    filter_prefix = "email"
-    filter_suffix = ".csv"
   }
 }
